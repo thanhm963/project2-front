265b7d4391448bae07db87ec8cccff82db59e753
Thanh Ma <thanhm963@gmail.com>
2024-06-03 19:49:23 +0700

    create homepage using bootstrap table

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './layout/Navbar';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Home/>
     </div>
-  );
+  );        
 }
 
 export default App;
