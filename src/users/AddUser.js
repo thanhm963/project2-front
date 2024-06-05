370ed5cf8f009533abd5f252d4a9eee3c4ef6042
Thanh Ma <thanhm963@gmail.com>
2024-06-05 20:55:13 +0700

    post data into database AXIOS POST

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,6 +1,10 @@
 import React, {useState} from 'react'
-
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
 export default function AddUser() {
+
+  let navigate=useNavigate()
+
     const [user, setUser]=useState({
         name:"",
         username:"",
@@ -12,11 +16,19 @@ export default function AddUser() {
     const onInputChange=(e)=>{
         setUser({...user, [e.target.name]: e.target.value})
     }
+
+    const onSubmit=async (e) => {
+      e.preventDefault();
+      await axios.post("http://localhost:8080/user",user);
+      navigate("/")
+
+    }
   return(
    <div className="container">
       <div className="row">
           <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
              <h2 className="text-center m-4">Register User</h2>
+             <form onSubmit={(e)=>onSubmit(e)}>
              <div className="mb-3">
                 <label htmlFor="Name" className="form-label">
                     Name
@@ -57,7 +69,8 @@ export default function AddUser() {
                 />
              </div>
              <button type="submit" className="btn btn-outline-primary">Submit</button>
-             <button type="submit" className="btn btn-outline-danger mx-2">Cancel</button>
+             <Link className="btn btn-outline-danger mx-2" to="/">Cancel</Link>
+             </form>
           </div>
       </div>
   </div>
